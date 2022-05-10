8fcbb35adfb21dda9ead80b77319df4d4227b299
Trever <trever@macbook-pro.myfiosgateway.com>
2022-05-09 21:58:24 -0400

    pruned junk

--- live/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $uri = $_SERVER['REQUEST_URI'];
-    $username = explode('/', $uri);
-    $username = end($username);
-    if ($username == "")
-        include './homepage.php';
-    else
-        include './liveview.php';
-?>
